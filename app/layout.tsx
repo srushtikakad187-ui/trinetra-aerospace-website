c687b49b1c24b7a442d6cef148ff9d62f1540408
Srushti Kakad <srushtikakad187@gmail.com>
2026-06-29 02:31:10 +0530

    Fix favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,24 +40,22 @@ export const metadata: Metadata = {
     type: 'website',
   },
   icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
-    apple: '/apple-icon.png',
-  },
+  icon: [
+    {
+      url: '/icon.png',
+    },
+    {
+      url: '/icon-light-32x32.png',
+      media: '(prefers-color-scheme: light)',
+    },
+    {
+      url: '/icon-dark-32x32.png',
+      media: '(prefers-color-scheme: dark)',
+    },
+  ],
+  apple: '/apple-icon.png',
+},
 }
-
 export const viewport: Viewport = {
   colorScheme: 'light',
   themeColor: '#F5F1E8',
